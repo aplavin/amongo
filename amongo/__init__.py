e7b6fd773379b754fedae6d9dcd0a42f882f954c
Alexander Plavin <me@aplavin.ru>
2013-04-07 13:43:49 +0400

    Initial commit: setup.py, and AMongoObject
    --HG--
    extra : convert_revision : 51fecc460adf850c7ae23f7db218ff7fa247a2ed

---
+++ amongo/__init__.py
@@ -0,0 +1,6 @@
+
+
+class AMongoObject(object):
+    def __init__(self, collection):
+        self.collection = collection
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='amongo',
+    version='',
+    packages=['amongo'],
+    url='',
+    license='',
+    author='alexander',
+    author_email='me@aplavin.ru',
+    description=''
+)
